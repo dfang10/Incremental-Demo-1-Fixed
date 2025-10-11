2c584c57098404c8d49d07517c9fc9d81cf64c3c
dfang10 <dfang10@ucsc.edu>
2025-10-11 00:13:03 +0000

    Step 5 completed, added upgrade purchase. Button disables when unable to afford and added ability to purchase 2 clickers at a time.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import "./style.css";
 
 let counter: number = 0; // Amount of potatos
+let costClicker: number = 10; // Base price for an auto clicker
+let clickerCount: number = 0; // Amount of auto clickers
 
 // Potato button
 const potato = document.createElement("div");
@@ -14,6 +16,32 @@ document.body.style.textAlign = "center";
 document.body.style.marginTop = "0px";
 document.body.appendChild(potato);
 
+// clicker button
+const clicker = document.createElement("button");
+clicker.className = "clicker";
+clicker.style.position = "absolute";
+clicker.style.cursor = "pointer";
+clicker.style.fontSize = "24px";
+clicker.textContent = `Buy auto clicker: ${costClicker}🍟`;
+
+clicker.style.textAlign = "center";
+clicker.style.marginTop = "275px";
+clicker.style.fontFamily = "cursive";
+document.body.appendChild(clicker);
+
+// Buy 2 clicker button
+const clicker2 = document.createElement("button");
+clicker2.className = "clicker2";
+clicker2.style.position = "absolute";
+clicker2.style.cursor = "pointer";
+clicker2.style.fontSize = "24px";
+clicker2.textContent = `Buy 2 auto clickers: ${costClicker * 2}🍟`;
+
+clicker2.style.textAlign = "center";
+clicker2.style.marginTop = "400px";
+clicker2.style.fontFamily = "cursive";
+document.body.appendChild(clicker2);
+
 // Display amount of fries
 const counterElement = document.createElement("p");
 counterElement.id = "click-counter";
@@ -25,6 +53,43 @@ counterElement.style.marginTop = "200px";
 
 document.body.appendChild(counterElement);
 
+updateButtons();
+
+// Function for buying auto clickers
+function buyClick() {
+  if (counter >= costClicker) {
+    counter -= costClicker;
+    costClicker++;
+    clickerCount++;
+    clicker.textContent = `Buy auto clicker: ${costClicker}🍟`;
+    clicker2.textContent = `Buy auto clicker: ${costClicker * 2}🍟`;
+    updateButtons();
+    console.log(clickerCount);
+  } else if (counter < costClicker) {
+    updateButtons();
+  }
+}
+
+function buyClick2() {
+  if (counter >= (costClicker * 2)) {
+    counter -= costClicker * 2;
+    costClicker += 2;
+    clickerCount += 2;
+    clicker.textContent = `Buy auto clicker: ${costClicker}🍟`;
+    clicker2.textContent = `Buy auto clicker: ${costClicker * 2}🍟`;
+    updateButtons();
+    console.log(clickerCount);
+  } else if (counter < (costClicker * 2)) {
+    updateButtons();
+  }
+}
+
+// Disable button when not enough
+function updateButtons() {
+  clicker.disabled = counter < costClicker;
+  clicker2.disabled = counter < (costClicker * 2);
+}
+
 // setInterval growth
 //setInterval(() => {
 //  counter++;
@@ -35,21 +100,23 @@ document.body.appendChild(counterElement);
 let lastTime = performance.now();
 function gameLoop(currentTime: number) {
   const deltaSec = (currentTime - lastTime) / 1000; // seconds passed
-  //if (clickerCount > 0) {
-  counter += 1 * deltaSec; // fractional growth
-  counterElement.textContent = `Fries 🍟: ${Math.floor(counter)}`;
-
-  //}
+  if (clickerCount > 0) {
+    counter += clickerCount * deltaSec; // fractional growth
+    counterElement.textContent = `Fries 🍟: ${Math.floor(counter)}`;
+    updateButtons();
+  }
   lastTime = currentTime;
   requestAnimationFrame(gameLoop);
 }
-requestAnimationFrame(gameLoop); // Start the loop!
+requestAnimationFrame(gameLoop); 
 
 // When potato button clicked
 potato.addEventListener("click", () => {
   // Update click count
   counter++;
   counterElement.textContent = `Fries 🍟: ${counter}`;
+  updateButtons();
+
 
   // Play animation
   potato.style.transform = "scale(0.8)";
@@ -61,6 +128,26 @@ potato.addEventListener("click", () => {
   for (let i = 0; i < 10; i++) {
     createSparkle();
   }
+});
+
+// When purchase auto clicker button is clicked
+clicker.addEventListener("click", () => {
+  buyClick();
+
+  // Animation
+  clicker.style.transform = "scale(0.8)";
+  setTimeout(() => {
+    clicker.style.transform = "scale(1)";
+  }, 100);
+});
+clicker2.addEventListener("click", () => {
+  buyClick2();
+
+  // Animation
+  clicker2.style.transform = "scale(0.8)";
+  setTimeout(() => {
+    clicker2.style.transform = "scale(1)";
+  }, 100);
 });
 
 // Function to create a sparkle particle (used brace to create this)
